1afc6756e0baa9fbb088802a4e73c7d740e2416d
josephlee3454 <joseph.lee3454@gmail.com>
2020-03-25 13:17:11 -0700

    started-lab12

---
+++ secondschema.sql
@@ -0,0 +1,1 @@
+INSERT INTO books (title, author, isbn, image_url, description, bookshelf) VALUES ('Wise Mans Fear, The', 'Patrick Rothfuss', '8675309999','https://images-na.ssl-images-amazon.com/images/I/51JThzjy3gL._SX306_BO1,204,203,200_.jpg', 'A good book or maybe the best book if your name is Rachael', 'bookshelf?');
--- server.js
+++ server.js
@@ -5,22 +5,35 @@ const ejs = require('ejs');// opens ejs library
 const express = require('express');//allows access to express library
 const app = express();// assigns app to express
 const superagent = require('superagent'); // assigns superagent so you can use it 
-
+const pg = require('pg');
+const client = new pg.Client(process.env.DATABASE_URL);//creating a new client in the database and having it look into the env file to find what port we are listening to 
 const PORT = process.env.PORT || 3001;//assigns varible to port number in env file
 app.set('view engine','ejs');// tells express to use ejs and to look for a veiws folder
 
 app.use(express.urlencoded({extended: true}));// 'this is the way it likes' ejs requires this to set a static page
 app.use(express.static('./public'));// telling it not to look for index but to look for public instead
 
-//searche/new is the door you create
+//searches/new is the door you create
 app.get('/searches/new', (request,response)=>{
   response.render('./pages/searches/new.ejs');
 })
 
 // overwriting the defaults such as index.html
-app.get('/', (request, response) => { // look in public
-  response.render('pages/index.ejs');// look for specifically index .ejs and serve that up
-})
+// need fake data in a database before this will work
+
+app.get('/', renderHomePage);
+function renderHomePage(request,response){
+  let SQL = 'SELECT * FROM books'
+  client.query(SQL)//clinet postgres we are making a query into the clinet and the query is SQL
+  .then(results=>{
+    let books = results.row;
+    let bookNumber = books.length;
+    response.render('/index.ejs', {booksArray: books,bookNumber})
+    // add a .catch
+
+  })
+}
+
 
 
 app.post('/searches', (request, response) => {//recives searches from front end 
@@ -48,7 +61,7 @@ app.post('/searches', (request, response) => {//recives searches from front end
     response.render('./pages/searches/show.ejs', {books:finalBookArray} )
   })
   .catch(error =>{   //catches errors
-    
+
     Error(error, response);
   });
     })
